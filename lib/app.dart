3a92dc3ca1155eb458fb050c967afdff6b385203
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-20 01:25:39 +0300

    add ScreenUtilInit in matrial and create color manager class

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Note Nest',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-    );
+    return ScreenUtilInit(
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) => MaterialApp(
+              builder: (context, widget) {
+                final mediaQueryData = MediaQuery.of(context);
+                final scaledMediaQueryData = mediaQueryData.copyWith(
+                  textScaler: TextScaler.noScaling,
+                );
+                return MediaQuery(
+                  data: scaledMediaQueryData,
+                  child: widget!,
+                );
+              },
+              title: 'Note Nest',
+              debugShowCheckedModeBanner: false,
+            ));
   }
 }
---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  const ColorManager._();
+  static const Color dark = Color(0xff252525);
+  static const Color grey = Color(0xff3B3B3B);
+  static const Color red = Color(0xffFF0000);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color green = Color(0xff30BE71);
+//? Notes Colors
+  static const Color magenta = Color(0xffFD99FF);
+  static const Color lightPink = Color(0xffFF9E9E);
+  static const Color lightGreen = Color(0xff91F48F);
+  static const Color lightYellow = Color(0xffFFF599);
+  static const Color lightBlue = Color(0xff9EFFFF);
+  static const Color lightpurple = Color(0xffB69CFF);
+}
