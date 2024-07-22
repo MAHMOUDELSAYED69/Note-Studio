6cbedc559be40b9a71b08b5c9d07a1658c9e54d9
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-22 17:35:28 +0300

    create custom search bar

---
+++ lib/utils/helper/search_bar.dart
@@ -0,0 +1,77 @@
+
+import 'package:flutter/material.dart';
+import 'package:note_studio/utils/extentions/extentions.dart';
+
+class NotesSearchDelegate extends SearchDelegate<String> {
+  final List<String> notes;
+
+  NotesSearchDelegate({required this.notes});
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(
+          Icons.clear,
+          color: context.iconTheme.color,
+        ),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.arrow_back,
+        color: context.iconTheme.color,
+      ),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final results = notes
+        .where((note) => note.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(results[index], style: context.textTheme.bodyLarge),
+          onTap: () {
+            close(context, results[index]);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestions = notes
+        .where((note) => note.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(
+            suggestions[index],
+            style: context.textTheme.bodyLarge,
+          ),
+          onTap: () {
+            query = suggestions[index];
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+}
