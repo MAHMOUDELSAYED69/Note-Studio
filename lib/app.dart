3f1a89351a4b6f987989f2408fca9a7590b1c458
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-24 00:35:38 +0300

    add provide cubits in matrial app, create PageTransitionManager

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../controllers/theme_cubit/theme_cubit.dart';
 import '../router/app_router.dart';
 import '../utils/constants/routes.dart';
 import '../utils/themes/app_theme.dart';
+import 'controllers/notes_cubit/notes_cubit.dart';
+import 'data/repository/note_repo.dart';
+import 'data/sql/sql.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,22 +17,39 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
         minTextAdapt: true,
         splitScreenMode: true,
-        builder: (_, child) => MaterialApp(
-              builder: (context, widget) {
-                final mediaQueryData = MediaQuery.of(context);
-                final scaledMediaQueryData = mediaQueryData.copyWith(
-                  textScaler: TextScaler.noScaling,
-                );
-                return MediaQuery(
-                  data: scaledMediaQueryData,
-                  child: widget!,
-                );
-              },
-              title: 'Note Nest',
-              theme: AppTheme.darkTheme,
-              debugShowCheckedModeBanner: false,
-              initialRoute: RouteManager.initialRoute,
-              onGenerateRoute: AppRouter.onGenerateRoute,
+        builder: (_, child) => MultiBlocProvider(
+              providers: [
+                BlocProvider(
+                  create: (context) =>
+                      NotesCubit(NotesRepository(SqlDatabase()))..fetchNotes(),
+                ),
+                BlocProvider(
+                  create: (context) => ThemeCubit(),
+                ),
+              ],
+              child: BlocBuilder<ThemeCubit, ThemeState>(
+                builder: (context, state) {
+                  return MaterialApp(
+                    builder: (context, widget) {
+                      final mediaQueryData = MediaQuery.of(context);
+                      final scaledMediaQueryData = mediaQueryData.copyWith(
+                        textScaler: TextScaler.noScaling,
+                      );
+                      return MediaQuery(
+                        data: scaledMediaQueryData,
+                        child: widget!,
+                      );
+                    },
+                    title: 'Note Studio',
+                    theme: state == ThemeState.dark
+                        ? AppTheme.darkTheme
+                        : AppTheme.lightTheme,
+                    debugShowCheckedModeBanner: false,
+                    initialRoute: RouteManager.initialRoute,
+                    onGenerateRoute: AppRouter.onGenerateRoute,
+                  );
+                },
+              ),
             ));
   }
 }
