0b6aa2455923f84a3d5c6240ed7d9fd8db61a16f
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-24 00:37:26 +0300

    modify app theme

--- lib/utils/themes/app_theme.dart
+++ lib/utils/themes/app_theme.dart
@@ -8,6 +8,9 @@ abstract class AppTheme {
   //!! dark THEME
   static ThemeData get darkTheme {
     return ThemeData(
+      cardColor: ColorManager.darkGrey,
+      primaryColor: ColorManager.dark,
+      canvasColor: ColorManager.grey,
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
           overlayColor:
@@ -19,20 +22,9 @@ abstract class AppTheme {
               borderRadius: BorderRadius.circular(8),
             ),
           ),
-          fixedSize: WidgetStatePropertyAll(
-            Size(double.maxFinite, 48.h),
-          ),
         ),
       ),
 
-      checkboxTheme: CheckboxThemeData(
-        checkColor: const WidgetStatePropertyAll(ColorManager.white),
-        fillColor: const WidgetStatePropertyAll(ColorManager.white),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(3.dm),
-        ),
-        side: BorderSide(color: ColorManager.darkGrey, width: 1.dm),
-      ),
       //-----------------------------------------------------------//* Floating Action Button
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
           foregroundColor: ColorManager.white,
@@ -41,15 +33,7 @@ abstract class AppTheme {
           splashColor: ColorManager.transparent),
       //-----------------------------------------------------------//* Icon
       iconTheme: const IconThemeData(color: ColorManager.white, size: 25),
-      switchTheme: const SwitchThemeData(
-        trackOutlineColor: WidgetStatePropertyAll(ColorManager.green),
-        thumbColor: WidgetStatePropertyAll(ColorManager.white),
-        trackColor: WidgetStatePropertyAll(ColorManager.green),
-        thumbIcon: WidgetStatePropertyAll(Icon(
-          Icons.dark_mode,
-          color: ColorManager.white,
-        )),
-      ),
+
       fontFamily: FontFamilyManager.nunito,
       useMaterial3: true,
       brightness: Brightness.dark,
@@ -81,7 +65,7 @@ abstract class AppTheme {
           fontWeight: FontWeight.w300,
         ),
         bodySmall: TextStyle(
-          fontSize: 14.spMin,
+          fontSize: 14.sp,
           color: ColorManager.white,
           fontWeight: FontWeight.w400,
         ),
@@ -95,13 +79,6 @@ abstract class AppTheme {
           fontSize: 21.sp,
           color: ColorManager.lightGrey,
         ),
-        displaySmall: TextStyle(
-          decoration: TextDecoration.underline,
-          decorationColor: ColorManager.green,
-          color: ColorManager.green,
-          fontSize: 14.spMin,
-          fontWeight: FontWeight.w500,
-        ),
       ),
 
       textSelectionTheme: TextSelectionThemeData(
@@ -109,7 +86,7 @@ abstract class AppTheme {
           selectionColor: ColorManager.darkGrey.withOpacity(0.7),
           selectionHandleColor: ColorManager.darkGrey),
 
-      //--------------------------------------------------//* INPUT DECORATION
+      //--------------------------------------------------//* INPUT DECORATION Text Field
       inputDecorationTheme: const InputDecorationTheme(
         border: InputBorder.none,
         contentPadding: EdgeInsets.zero,
@@ -118,6 +95,99 @@ abstract class AppTheme {
         errorBorder: OutlineInputBorder(borderSide: BorderSide.none),
         focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide.none),
       ),
+      //--------------------------------------------------//* Search Bar
+    );
+  }
+
+  //?? light THEME
+  static ThemeData get lightTheme {
+    return ThemeData(
+      cardColor: ColorManager.lightGrey,
+      primaryColor: ColorManager.lightGrey,
+      canvasColor: ColorManager.dark,
+
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          overlayColor:
+              WidgetStatePropertyAll(ColorManager.white.withOpacity(0.2)),
+          foregroundColor: const WidgetStatePropertyAll(ColorManager.white),
+          backgroundColor: const WidgetStatePropertyAll(ColorManager.green),
+          shape: WidgetStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ),
+
+      //-----------------------------------------------------------//* Floating Action Button
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          foregroundColor: ColorManager.dark,
+          backgroundColor: ColorManager.white,
+          shape: CircleBorder(),
+          splashColor: ColorManager.transparent),
+      //-----------------------------------------------------------//* Icon
+      iconTheme: const IconThemeData(color: ColorManager.dark, size: 25),
+      fontFamily: FontFamilyManager.nunito,
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: ColorManager.white,
+      //-----------------------------------------------------------//* APP BAR
+      appBarTheme: AppBarTheme(
+        titleTextStyle: TextStyle(
+            color: ColorManager.dark,
+            fontSize: 37.sp,
+            fontFamily: FontFamilyManager.nunito),
+        backgroundColor: ColorManager.transparent,
+        scrolledUnderElevation: 0,
+        titleSpacing: 0,
+        iconTheme: const IconThemeData(color: ColorManager.dark),
+        elevation: 0,
+      ),
+
+      //-----------------------------------------------------------//* TEXT
+      textTheme: TextTheme(
+        bodyLarge: TextStyle(
+          fontSize: 30.sp,
+          color: ColorManager.dark,
+          fontWeight: FontWeight.w600,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 17.5.sp,
+          color: ColorManager.dark,
+          fontWeight: FontWeight.w300,
+        ),
+        bodySmall: TextStyle(
+          fontSize: 14.sp,
+          color: ColorManager.dark,
+          fontWeight: FontWeight.w400,
+        ),
+
+        displayLarge: TextStyle(
+          fontSize: 45.2.sp,
+          color: ColorManager.darkGrey,
+        ),
+        //--------------------------------------------------//* For button
+        displayMedium: TextStyle(
+          fontSize: 21.sp,
+          color: ColorManager.darkGrey,
+        ),
+      ),
+      textSelectionTheme: TextSelectionThemeData(
+          cursorColor: ColorManager.darkGrey,
+          selectionColor: ColorManager.lightGrey.withOpacity(0.7),
+          selectionHandleColor: ColorManager.lightGrey),
+
+      //--------------------------------------------------//* INPUT DECORATION Text Field
+      inputDecorationTheme: const InputDecorationTheme(
+        border: InputBorder.none,
+        contentPadding: EdgeInsets.zero,
+        focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        errorBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide.none),
+      ),
+      //--------------------------------------------------//* Search Bar
     );
   }
 }
